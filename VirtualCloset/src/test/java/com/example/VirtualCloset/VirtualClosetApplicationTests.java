717c3be1b633892a3841bda3149b45ad7791b8ec
Valeri/Hriho <91058629+VHriho@users.noreply.github.com>
2023-12-01 01:25:20 +0200

    Add files via upload

---
+++ VirtualCloset/src/test/java/com/example/VirtualCloset/VirtualClosetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VirtualCloset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualClosetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
